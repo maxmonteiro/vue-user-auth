c6dc5c7bbef5bba2174a9e53eb897e076858f916
Maximo <maxmonteiro86@gmail.com>
2018-08-15 21:41:38 -0300

    Adicionando CORS no Laravel

--- vue-laravel/src/App.vue
+++ vue-laravel/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+          <app-new-user></app-new-user>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NewUser from './components/new-user.vue'
+
 export default {
   data () {
     return {
 
     }
+  },
+  components: {
+    'app-new-user': NewUser
   }
 }
 </script>
